fd3b8ad4d7350120e5c5384b5da907a8ac3516ae
Tom Swope <tjswope@gmail.com>
2024-12-10 14:35:46 -0500

    Initial Commit

---
+++ NewAPI2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewAPI2
+//
+//  Created by Swope, Thomas on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewAPI2/NewAPI2App.swift
@@ -0,0 +1,17 @@
+//
+//  NewAPI2App.swift
+//  NewAPI2
+//
+//  Created by Swope, Thomas on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewAPI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
