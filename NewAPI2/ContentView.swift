1561f81137d0eaeb309f97fda1798a90e58dce6f
Thomas Swope <35576679+tjswope@users.noreply.github.com>
2024-12-10 14:44:23 -0500

    this is very sad.

--- NewAPI2/ContentView.swift
+++ NewAPI2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("goodbye, world!")
+            Text("goodbye cruel, world!")
         }
         .padding()
     }
